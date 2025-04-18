4cda799c9260fcadf9a112227b8945c419808844
Chairoel <projectchairul@gmail.com>
2025-04-19 03:07:08 +0700

    create action and reducer redux for authUser state

--- src/states/authUser/action.js
+++ src/states/authUser/action.js
@@ -1,3 +1,56 @@
+import api from "../../utils/api";
+
 /**
  * @TODO: Define all the actions (creator) for the authUser state
  */
+const ActionType = {
+  SET_AUTH_USER: "SET_AUTH_USER",
+  UNSET_AUTH_USER: "UNSET_AUTH_USER",
+};
+
+// set payload: { authUser: authUser }
+function setAuthUserActionCreator(authUser) {
+  return {
+    type: ActionType.SET_AUTH_USER,
+    payload: { authUser },
+  };
+}
+
+// set payload: { authUser: null }
+function unsetAuthUserActionCreator() {
+  return {
+    type: ActionType.UNSET_AUTH_USER,
+    payload: {
+      authUser: null,
+    },
+  };
+}
+
+function asyncSetAuthUser({ id, password }) {
+  return async (dispatch) => {
+    try {
+      const token = await api.login({ id, password });
+      api.putAccessToken(token);
+      const authUser = await api.getOwnProfile();
+
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+function asyncUnsetAuthUser() {
+  return (dispatch) => {
+    dispatch(unsetAuthUserActionCreator());
+    api.putAccessToken();
+  };
+}
+
+export {
+  ActionType,
+  setAuthUserActionCreator,
+  unsetAuthUserActionCreator,
+  asyncSetAuthUser,
+  asyncUnsetAuthUser,
+};
