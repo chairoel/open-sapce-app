2237c0d02d27c16ce0ce49d0c93da829e53a731f
Chairoel <projectchairul@gmail.com>
2025-04-19 15:02:22 +0700

    create redux store

--- src/states/index.js
+++ src/states/index.js
@@ -1,3 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authUserReducer from "./authUser/reducer";
+import isPreLoadReducer from "./isPreload/reducer";
+import usersReducer from "./users/reducer";
+import talksReducer from "./talks/reducer";
+import talkDetailReducer from "./talkDetail/reducer";
+
 /**
  * @TODO: Create Redux store
  */
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreLoad: isPreLoadReducer,
+    users: usersReducer,
+    talks: talksReducer,
+    talkDetail: talkDetailReducer,
+  },
+});
+
+export default store;
